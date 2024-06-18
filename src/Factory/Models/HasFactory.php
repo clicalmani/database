08b800602709ed3534cf102237f46f70c0740e05
clicalmani <honorable85@gmail.com>
2024-06-18 09:42:16 +0100

    Back compatibility for php 8.2.*

--- src/Factory/Models/HasFactory.php
+++ src/Factory/Models/HasFactory.php
@@ -5,7 +5,7 @@ trait HasFactory
 {
     public static function seed()
     {
-        $className = get_class();
+        $className = get_called_class();
         $model = substr($className, strrpos($className, "\\") + 1);
         
         $factory = "\\Database\\Factories\\" . $model . 'Factory';
